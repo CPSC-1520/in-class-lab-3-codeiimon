05355ac540fcaf14ef3e3dbe9d47ac12249cab0a
Itua Imonikhe <hello.iimon@tutamail.com>
2024-10-02 22:41:46 -0600

    Complete Exercise 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 // Enter your code below.
+
+// Exercise 1
+let jsRes = document.querySelector(".javascript-resources")
+let showResButton = document.querySelector("#show-resources")
+console.log(jsRes)
+
+function showResEventHandler() {
+    jsRes.classList.remove("d-none");
+}
+
+showResButton.addEventListener("click", showResEventHandler)
