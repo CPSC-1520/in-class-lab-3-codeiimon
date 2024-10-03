c7d2591448607920947589e66158ceeee42c198d
Itua Imonikhe <hello.iimon@tutamail.com>
2024-10-02 23:05:24 -0600

    Add Semicolons

--- js/main.js
+++ js/main.js
@@ -1,33 +1,32 @@
 // Enter your code below.
 
 // Exercise 1
-let jsRes = document.querySelector(".javascript-resources")
-let showResButton = document.querySelector("#show-resources")
-console.log(jsRes)
+let jsRes = document.querySelector(".javascript-resources");
+let showResButton = document.querySelector("#show-resources");
 
 function showResClickEventHandler() {
     jsRes.classList.remove("d-none");
-}
+};
 
-showResButton.addEventListener("click", showResClickEventHandler)
+showResButton.addEventListener("click", showResClickEventHandler);
 
 // Exercise 2
 function jsResMouseOverEventHandler(event) {
-    event.target.classList.add("fw-bold")
-}
+    event.target.classList.add("fw-bold");
+};
 
-jsRes.addEventListener("mouseover", jsResMouseOverEventHandler)
+jsRes.addEventListener("mouseover", jsResMouseOverEventHandler);
 
 // Exercise 3
 function jsResMouseOutEventHandler(event) {
-    event.target.classList.remove("fw-bold")
-}
+    event.target.classList.remove("fw-bold");
+};
 
-jsRes.addEventListener("mouseout", jsResMouseOutEventHandler)
+jsRes.addEventListener("mouseout", jsResMouseOutEventHandler);
 
 // Exercise 4
 function jsResClickEventHandler(event) {
-   event.target.classList.add("fst-italic")
-}
+   event.target.classList.add("fst-italic");
+};
 
-jsRes.addEventListener("click", jsResClickEventHandler)
+jsRes.addEventListener("click", jsResClickEventHandler);
